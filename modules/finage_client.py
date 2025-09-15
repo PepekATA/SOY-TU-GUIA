766f8e3dcc73e7a97774ea649b3f9c67c093a597
PepekATA <122450179+PepekATA@users.noreply.github.com>
2025-09-15 16:39:34 -0400

    Update finage_client.py

--- modules/finage_client.py
+++ modules/finage_client.py
@@ -1,12 +1,11 @@
-import requests
 import os
+import requests
 
 API_KEY = os.getenv("FINAGE_API_KEY")
 BASE_URL = "https://api.finage.co.uk/forex/signal"
 
 def get_forex_signal(symbol: str):
-    """Llama a la API de Finage para obtener señales de predicción Forex"""
+    """Obtiene la señal de predicción de un par de divisas"""
     url = f"{BASE_URL}?symbol={symbol}&apikey={API_KEY}"
     response = requests.get(url)
-    data = response.json()
-    return data
+    return response.json()
