46705d56096115ed2e432608c723ea95f80ef031
PepekATA <122450179+PepekATA@users.noreply.github.com>
2025-09-16 00:28:26 -0400

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,20 @@
+# modules/__init__.py
+# Hace que "modules" sea un paquete importable
 
+from .ml_agents import BaseAgent, TrendAgent, MomentumAgent, VolatilityAgent, PatternAgent, ScalpingAgent, NewsAgent
+from .coordinator import MasterCoordinator
+from .system import ForexMultiAgentSystem
+from .alpaca_client import AlpacaRealClient
+
+__all__ = [
+    'BaseAgent',
+    'TrendAgent',
+    'MomentumAgent',
+    'VolatilityAgent',
+    'PatternAgent',
+    'ScalpingAgent',
+    'NewsAgent',
+    'MasterCoordinator',
+    'ForexMultiAgentSystem',
+    'AlpacaRealClient'
+]
